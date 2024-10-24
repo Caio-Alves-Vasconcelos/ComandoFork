6481960dfc990d754103a74f2432cac8949f831c
CaioAlves <0000983806@senaimgaluno.com.br>
2024-10-24 15:12:10 -0300

    Exercício 05

--- ConhendoFork/src/Ex05.java
+++ ConhendoFork/src/Ex05.java
@@ -1,10 +1,19 @@
 /*Simulação de Loteria: Gere 6 números aleatórios entre 1 e 60 e imprima-os.
  */
+// PC 5 - Caio Alves
+
+import java.util.Random;
+
 public class Ex05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
 
+		System.out.println("Números sorteados: ");
+		for (int i = 1; i <= 6; i++) {
+			int loteria = random.nextInt(60);
+			System.out.println(loteria);
+		}
 	}
 
 }
